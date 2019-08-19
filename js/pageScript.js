5136681b51b6c62479e49d1a1b8af05638a1a434
ZignOut <kaunghtut703@gmail.com>
2019-08-19 11:06:50 +0630

    final commit

--- js/pageScript.js
+++ js/pageScript.js
@@ -8,36 +8,45 @@ $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.popularmya
     $(".internationalNews .title").html(data.feed.title);
     $(".internationalNews .head").html(data.items[0].title);
     $(".internationalNews .content").html(data.items[0].content);
-    $(".localNews a").attr("href", data.items[0].guid)
+    $(".internationalNews a").attr("href", data.items[0].guid)
 });
 $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.popularmyanmar.com%2Fpopularnews%2Fcategory%2Fbussiness%2Ffeed', function(data){
     $(".weatherNews .title").html(data.feed.title);
     $(".weatherNews .head").html(data.items[0].title);
     $(".weatherNews .content").html(data.items[0].content);
-    $(".localNews a").attr("href", data.items[0].guid)
+    $(".weatherNews a").attr("href", data.items[0].guid);
 });
-// $(document).ready(function() {
-// 	//feed to parse
-// 	var feed = "https://api.rss2json.com/v1/api.json?rss_url=" + "https://www.popularmyanmar.com/rss";
-	
-// 	$.ajax(feed, {
-// 		accepts:{
-// 			xml:"application/json"
-// 		},
-// 		dataType:"xml",
-// 		success:function(data) {
-// 			//Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+$.get('https://api.rss2json.com/v1/api.json?rss_url=https://www.irrawaddy.com/category/news/feed', function(data){
+    $(".ayalocalNews .title").html(data.feed.title);
+    $(".ayalocalNews .head").html(data.items[0].title);
+    $(".ayalocalNews .content").html(data.items[0].content);
+    $(".ayalocalNews a").attr("href", data.items[0].link);
+});
 
-// 			$(data).find("item").each(function () { // or "item" or whatever suits your feed
-// 				var el = $(this);
-// 				console.log("------------------------");
-// 				console.log("title      : " + el.find("title").text());
-// 				console.log("link       : " + el.find("link").text());
-// 				console.log("description: " + el.find("description").text());
-// 			});
+var curDate = new Date();
+$(".date").html(curDate);
+/*$(document).ready(function() {
+	//feed to parse
+	var feed = "https://api.rss2json.com/v1/api.json?rss_url=" + "https://www.popularmyanmar.com/rss";
+	
+	$.ajax(feed, {
+		accepts:{
+			xml:"application/json"
+		},
+		dataType:"xml",
+		success:function(data) {
+			//Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+
+			$(data).find("item").each(function () { // or "item" or whatever suits your feed
+				var el = $(this);
+				console.log("------------------------");
+				console.log("title      : " + el.find("title").text());
+				console.log("link       : " + el.find("link").text());
+				console.log("description: " + el.find("description").text());
+			});
 	
 
-// 		}	
-// 	});
+		}	
+	});
 	
-// });
+});*/
